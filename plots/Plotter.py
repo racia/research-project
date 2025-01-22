853985edb12ea768ac1d5293c69193c95d77f772
ivakhnenko <ivakhnenko@cl.uni-heidelberg.de>
2025-01-22 13:44:01 +0000

    Fix and improve the plotting script

--- plots/Plotter.py
+++ plots/Plotter.py
@@ -23,12 +23,78 @@ class Plotter:
         self.plot_counter_task = 0
         self.plot_counter_prompt = 0
 
+    def _save_plot(
+        self,
+        y_label: str,
+        x_label: str,
+        file_name: str,
+        plot_name_add: str,
+    ) -> None:
+        """
+        Save the plot to a file.
+
+        :param y_label: label for the y-axis, i.e. the type of data
+        :param x_label: label for the x-axis, i.e. the data for testing
+        :param file_name: name of the file
+        :param plot_name_add: addition to the plot name
+
+        :return: None
+        """
+        if file_name is not None:
+            plt.savefig(file_name, bbox_inches="tight")
+        else:
+            label = y_label.lower().replace(" ", "_")
+            plt.savefig(
+                self.result_path
+                / f"{plot_name_add}{label}_per_{x_label.lower()}_no_{self.plot_counter_task}.png",
+                bbox_inches="tight",
+            )
+
+        self.plot_counter_prompt += 1
+        plt.close()
+
+    @staticmethod
+    def _plot_general_details(
+        x_label: str,
+        y_label: str,
+        max_x_len: int,
+        plot_name_add: str,
+        compare: bool = False,
+    ) -> None:
+        """
+        Plot the general details of the plot, e.g. labels, title, and legend.
+
+        :param x_label: label for the x-axis
+        :param y_label: label for the y-axis
+        :param max_x_len: maximum length of the x-axis
+        :param plot_name_add: addition to the plot name
+        :param compare: whether the data is compared
+        :return: None
+        """
+        plt.xticks(range(1, max_x_len + 1))
+        plt.xlabel(x_label)
+
+        plt.ylim(bottom=0, top=1)
+        type_of_data = " ".join([part.capitalize() for part in y_label.split("_")])
+        plt.ylabel(type_of_data)
+
+        title = f"{type_of_data} per {x_label}"
+        if compare:
+            title += " and prompt"
+        if plot_name_add:
+            title += f" ({plot_name_add.strip('_')})"
+        plt.title(title)
+
+        plt.legend(
+            loc="center left", bbox_to_anchor=(1, 0.5), fancybox=True, shadow=True
+        )
+
     def plot_acc_per_task(
         self,
         acc_per_task: list,
         x_label: str = "Task",
         y_label: str = "Accuracy",
-        plot_name=None,
+        file_name=None,
         plot_name_add: str = "",
     ) -> None:
         """
@@ -37,42 +103,25 @@ class Plotter:
         :param acc_per_task: list of accuracies per task. We assume that the list is ordered ascending by task.
         :param x_label: label for the x-axis
         :param y_label: label for the y-axis
-        :param plot_name: name of the plot
+        :param file_name: name of the plot
         :param plot_name_add: addition to the plot name
         :return: None
         """
         plt.figure(figsize=(10, 5))
-
-        # make the plots prettier
         colors = self.cmap(np.linspace(0, 1, len(acc_per_task)))
-
         plt.plot(range(1, len(acc_per_task) + 1), acc_per_task, color=colors[0])
-        plt.xticks(range(1, len(acc_per_task) + 1))
-        plt.xlabel(x_label)
-        plt.ylabel(y_label)
 
-        plt.title(f"{y_label} per {x_label}")
-        if plot_name_add:
-            plt.title(f"{y_label} per {x_label} ({plot_name_add.strip('_')})")
-
-        if plot_name is not None:
-            plt.savefig(plot_name)
-        else:
-            label = y_label.lower().replace(" ", "_")
-            plt.savefig(
-                self.result_path
-                / f"{plot_name_add}{label}_per_task_no_{self.plot_counter_task}.png"
-            )
-
-        self.plot_counter_task += 1
-        plt.close()
+        self._plot_general_details(
+            x_label, y_label, len(acc_per_task), plot_name_add, compare=False
+        )
+        self._save_plot(y_label, x_label, file_name, plot_name_add)
 
     def plot_acc_per_task_and_prompt(
         self,
         acc_per_prompt_task: dict[str, list],
         x_label: str = "Task",
         y_label: str = "Accuracy",
-        plot_name=None,
+        file_name=None,
         plot_name_add: str = "",
     ) -> None:
         """
@@ -82,54 +131,20 @@ class Plotter:
         task.
         :param x_label: label for the x-axis
         :param y_label: label for the y-axis
-        :param plot_name: name of the plot
+        :param file_name: name of the plot
         :param plot_name_add: addition to the plot name
         :return: None
         """
         plt.figure(figsize=(15, 5))
-
-        # make the plots prettier
         colors = self.cmap(np.linspace(0, 1, len(acc_per_prompt_task)))
 
         max_x_len = 0
-
         for (prompt, acc), color in zip(acc_per_prompt_task.items(), colors):
             if len(acc) > max_x_len:
                 max_x_len = len(acc)
             plt.plot(range(1, len(acc) + 1), acc, label=prompt, color=color)
 
-        plt.xticks(range(1, max_x_len + 1))
-        plt.xlabel(x_label)
-        plt.ylabel(y_label)
-
-        plt.title(
-            f"{y_label} per {x_label} and prompt",
+        self._plot_general_details(
+            x_label, y_label, len(acc_per_prompt_task), plot_name_add, compare=True
         )
-        if plot_name_add:
-            plt.title(f"{y_label} per {x_label} ({plot_name_add.strip('_')})")
-
-        # Locate the legend on the right side in the free space
-        plt.legend(
-            loc="center left",
-            bbox_to_anchor=(1, 0.5),
-            fancybox=True,
-            shadow=True,
-        )
-
-        plt.title(f"{y_label} per {x_label} and prompt")
-        if plot_name_add:
-            plt.title(f"{y_label} per {x_label} ({plot_name_add.strip('_')})")
-
-        plt.legend()
-        if plot_name is not None:
-            plt.savefig(plot_name, bbox_inches="tight")
-        else:
-            label = y_label.lower().replace(" ", "_")
-            plt.savefig(
-                self.result_path
-                / f"{plot_name_add}{label}_per_{x_label.lower()}_and_prompt_no_{self.plot_counter_task}.png",
-                bbox_inches="tight",
-            )
-
-        self.plot_counter_prompt += 1
-        plt.close()
+        self._save_plot(y_label, x_label, file_name, plot_name_add)
