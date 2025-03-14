83432eac865290aeb738c61fbb9b51bb5366f5ed
ivakhnenko <ivakhnenko@cl.uni-heidelberg.de>
2025-03-14 22:05:14 +0100

    Introduce the SamplePart class

--- interpretability/Interpretability.py
+++ interpretability/Interpretability.py
@@ -1,16 +1,11 @@
-
 from __future__ import annotations
 
-import os
-import re
-from typing import List
-import numpy as np
-from prompts.Chat import Chat
 import torch
 
-from plots.Plotter import Plotter
-from settings.Model import Model
 from interpretability.utils import get_attn_toks, is_stop_word
+from plots.Plotter import Plotter
+from prompts.Chat import Chat, SamplePart
+from settings.Model import Model
 
 
 class Interpretability:
@@ -24,8 +19,6 @@ class Interpretability:
         self.model = model.model
         self.tokenizer = model.tokenizer
         self.plotter = plotter
-    
-            
 
     def get_attention_scores(self, outputs, prompt_len, question_len, cot_len):
         """
@@ -37,48 +30,45 @@ class Interpretability:
         :param cot_len: CoT length
         :param attn_scores: 2D normalized attention scores averaged over layers and heads for CoT and question tokens.
         """
-        attn_tensor = torch.stack(outputs['attentions'], dim=0).squeeze(1)
+        attn_tensor = torch.stack(outputs["attentions"], dim=0).squeeze(1)
         print(attn_tensor)
         attn_tensor = attn_tensor.mean(dim=0)
-        attn_scores = attn_tensor[:, prompt_len:, prompt_len:].float().detach().cpu().numpy() # w/o system prompt
+        attn_scores = (
+            attn_tensor[:, prompt_len:, prompt_len:].float().detach().cpu().numpy()
+        )  # w/o system prompt
         print("SHAPE: ", attn_scores.shape, question_len, cot_len, prompt_len)
         attn_scores = attn_scores[:, question_len:cot_len, :question_len].mean(axis=0)
         print("SHAPE2: ", attn_scores.shape)
-        attn_scores = attn_scores/attn_scores.sum(axis=-1, keepdims=True)
+        attn_scores = attn_scores / attn_scores.sum(axis=-1, keepdims=True)
         print("SHAPE3", attn_scores.shape)
         assert attn_scores is not None
         print("SXOC", attn_scores)
         return attn_scores
 
-    
-    def calculate_attention(self, part_result: dict, chat: Chat):
+    def calculate_attention(self, part: SamplePart, chat: Chat):
         """
         Taken by CoT repo with changes:  Obtains attention scores through output attention weights as scores for CoT and Question for each part answer.
-        :param task_id: Current task id
-        :param part_id: Current sample part id
-        :param question: The question of sample part
-        :param reasoning: According reasoning of sample part
-        :param answer: According model answer                                                                                                        
-        :chat: Chat history as list of messages
+        :param part: part instance of the sample
+        :param chat: Chat history as list of messages
         :return: attention scores, tokenized x and y tokens
         """
 
-        task_id = part_result["task_id"]
-        sample_id = part_result["sample_no"]
-        part_id = part_result["part_id"]
-
-        question = part_result["part"]
-        reasoning = part_result["model_reasoning"]
-        answer = part_result["model_answer"]
-
-        input_parts = chat.interpr_parse_chat(question, reasoning, answer, model=self.model, tokenizer=self.tokenizer)
+        input_parts = chat.interpr_parse_chat(
+            part.question,
+            part.reasoning,
+            part.answer,
+            model=self.model,
+            tokenizer=self.tokenizer,
+        )
 
         input_msg = input_parts["input_msg"]
         question_len = input_parts["question_len"]
         prompt_len = input_parts["prompt_len"]
         cot_len = input_parts["cot_len"]
 
-        input_ids = chat.interp_build_chat_input(messages=input_msg, tokenizer=self.tokenizer)  
+        input_ids = chat.interp_build_chat_input(
+            messages=input_msg, tokenizer=self.tokenizer
+        )
         print("ids: ", input_ids)
         outputs = self.model(
             input_ids=input_ids.to(self.model.device),
@@ -86,10 +76,12 @@ class Interpretability:
             output_attentions=True,
             output_hidden_states=False,
         )
-        #print("out:", outputs)
+        # print("out:", outputs)
         input_ids = input_ids[0, prompt_len:].detach().cpu().numpy()
         print("ids: ", input_ids)
-        attn_scores = self.get_attention_scores(outputs, prompt_len, question_len, cot_len)
+        attn_scores = self.get_attention_scores(
+            outputs, prompt_len, question_len, cot_len
+        )
         print("scores", attn_scores)
         # Obtain attention tokens
         attn_tokens = get_attn_toks()
@@ -98,24 +90,38 @@ class Interpretability:
         for cot_row in attn_scores:
             print("cot_row", cot_row)
             for ind, attn in enumerate(cot_row):
-                tok = self.tokenizer.batch_decode(input_ids)[ind] 
+                tok = self.tokenizer.batch_decode(input_ids)[ind]
                 tok = tok.strip()
                 print(tok)
                 if is_stop_word(tok, attn_tokens):
                     not_att.append(ind)
 
-        attn_indices = list(filter(lambda x: x not in not_att, range(attn_scores.shape[1])))
+        attn_indices = list(
+            filter(lambda x: x not in not_att, range(attn_scores.shape[1]))
+        )
 
         attn_scores = attn_scores[:, attn_indices]
-        
-        print(attn_indices, attn_scores)
-        y_tokens = self.tokenizer.batch_decode(input_ids[question_len:cot_len] ) 
-        x_tokens = self.tokenizer.batch_decode(input_ids[attn_indices]) 
 
-        self.plotter.draw_heat(index=x_tokens, x=x_tokens, y=y_tokens, scores=attn_scores, task_id=task_id, sample_id=sample_id, part_id=part_id)
+        print(attn_indices, attn_scores)
+        y_tokens = self.tokenizer.batch_decode(input_ids[question_len:cot_len])
+        x_tokens = self.tokenizer.batch_decode(input_ids[attn_indices])
+
+        self.plotter.draw_heat(
+            index=x_tokens,
+            x=x_tokens,
+            y=y_tokens,
+            scores=attn_scores,
+            task_id=part.task_id,
+            sample_id=part.sample_id,
+            part_id=part.part_id,
+        )
         torch.cuda.empty_cache()
 
-        interpr_results = {"attn_scores": attn_scores, "x_tokens": x_tokens, "y_tokens": y_tokens}
-
-        return interpr_results
-   
+        # TO DISCUSS: would be better to save the interpretability in files
+        # interpr_results = {
+        #     "attn_scores": attn_scores,
+        #     "x_tokens": x_tokens,
+        #     "y_tokens": y_tokens,
+        # }
+        #
+        # return interpr_results
--- prompts/Chat.py
+++ prompts/Chat.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 from typing import List, Union
+
 import torch
 
 
@@ -14,6 +15,69 @@ class Source:
     system = "system"
     user = "user"
     assistant = "assistant"
+
+
+class SamplePart:
+    def __init__(
+        self,
+        id_: int,
+        task_id: int,
+        sample_id: int,
+        part_id: int,
+        context: str,
+        question: str,
+        reasoning: str,
+        answer: str,
+        golden_answer: str,
+        silver_reasoning=None,
+        tokenizer=None,
+    ):
+        self.id_ = id_
+        self.task_id = task_id
+        self.sample_id = sample_id
+        self.part_id = part_id
+
+        self.context = context
+        self.question = question
+        self.reasoning = reasoning
+        self.answer = answer
+
+        self.golden_answer = golden_answer
+        self.silver_reasoning = silver_reasoning
+
+        self.model_output = None
+        self.text = f"{context}\n{question}\n{reasoning}\n{answer}".strip()
+
+        self.tokenizer = tokenizer
+
+        if self.tokenizer:
+            self.context_ids = self.tokenize(self.context)
+            self.question_ids = self.tokenize(self.question)
+            self.reasoning_ids = self.tokenize(self.reasoning)
+            self.answer_ids = self.tokenize(self.answer)
+
+            self.context_ids_len = len(self.context_ids)
+            self.question_ids_len = len(self.question_ids)
+            self.reasoning_ids_len = len(self.reasoning_ids)
+            self.answer_ids_len = len(self.answer_ids)
+
+    def tokenize(self, sentence: str):
+        return self.tokenizer(sentence, return_tensors="pt").input_ids[0]
+
+    def get_result(self):
+        return {
+            "id": self.id_,
+            "task_id": self.task_id,
+            "sample_id": self.sample_id,
+            "part_id": self.part_id,
+            "context": self.context,
+            "question": self.question,
+            "reasoning": self.reasoning,
+            "answer": self.answer,
+            "golden_answer": self.golden_answer,
+            "silver_reasoning": self.silver_reasoning,
+            "model_output": self.model_output,
+        }
 
 
 class Chat:
@@ -54,14 +118,16 @@ class Chat:
         """
         if type(part) is list:
             part = "\n".join(part)
-        return {"role": role, "content": part}
-
+        return {
+            "role": role,
+            "content": part,
+        }
 
     def add_message(
         self,
         part: str | list[str],
         source: Union[Source.user, Source.assistant],
-        model_role: str = "student"
+        model_role: str = "student",
     ) -> None:
         """
         Add a message to the messages list.
@@ -75,11 +141,10 @@ class Chat:
         else:
             self.messages.append(self.format_message(part, source))
 
-
     def interpr_parse_messages(self, messages, split_role="user"):
         """
         Parses messages by appending new message in message_round list to message_rounds after initial system message.
-        :param messages: prompt messages 
+        :param messages: prompt messages
         :param split_role: one of system/user
         :return: system message and message_rounds list
         """
@@ -98,17 +163,24 @@ class Chat:
             message_rounds.append(message_round)
         return system_msg, message_rounds
 
-
-    def interp_build_chat_input(self, messages: List[dict], max_new_tokens: int=100, max_length: int=200, tokenizer=None):
+    def interp_build_chat_input(
+        self,
+        messages: List[dict],
+        max_new_tokens: int = 100,
+        max_length: int = 200,
+        tokenizer=None,
+    ):
         """
         As taken by CoT repo: Builds chat input by concatenating it left-wise to current chat messages excluding last concatenated model output.
         :param messages: List containing chat hisory
         :param max_new_tokens: max_new_tokens model config
-        :param max_length: default max_length of model config 
+        :param max_length: default max_length of model config
         :return: tensor of input tokens
-        """        
+        """
         max_input_tokens = max_length - max_new_tokens
-        system, rounds = self.interpr_parse_messages(messages, split_role="user") # Exclude last model answer, i.e. assistant msg
+        system, rounds = self.interpr_parse_messages(
+            messages, split_role="user"
+        )  # Exclude last model answer, i.e. assistant msg
         system_tokens = tokenizer.encode(system)
         max_history_tokens = max_input_tokens - len(system_tokens)
 
@@ -121,7 +193,10 @@ class Chat:
                 else:
                     round_tokens.append(tokenizer.convert_tokens_to_ids("assistant"))
                 round_tokens.extend(tokenizer.encode(message["content"]))
-            if len(history_tokens) == 0 or len(history_tokens) + len(round_tokens) <= max_history_tokens:
+            if (
+                len(history_tokens) == 0
+                or len(history_tokens) + len(round_tokens) <= max_history_tokens
+            ):
                 history_tokens = round_tokens + history_tokens  # concat left
                 if len(history_tokens) < max_history_tokens:
                     continue
@@ -129,13 +204,15 @@ class Chat:
 
         input_tokens = system_tokens + history_tokens
         if messages[-1]["role"] != "assistant":
-            input_tokens.append(tokenizer.convert_tokens_to_ids("assistant")) #model.generation_config.assistant_token_id
+            input_tokens.append(
+                tokenizer.convert_tokens_to_ids("assistant")
+            )  # model.generation_config.assistant_token_id
         input_tokens = input_tokens[-max_input_tokens:]  # truncate left
         return torch.LongTensor([input_tokens])
-    
 
-    def interpr_parse_chat(self, question: str, reasoning: str, answer: str, model, tokenizer):
-        
+    def interpr_parse_chat(
+        self, question: str, reasoning: str, answer: str, model, tokenizer
+    ):
         """
         Parses chat by providing contents and lengths of its parts.
         :param question: model question
@@ -145,18 +222,28 @@ class Chat:
         :return: info dict of chat parts
         """
         question_len = len(tokenizer(question, return_tensors="pt").input_ids[0])
-        question_msg = self.messages[:-1] # old chat is self.messages
+        question_msg = self.messages[:-1]  # old chat is self.messages
         print("Q msg", question_msg)
         question_ids = self.interp_build_chat_input(question_msg, tokenizer=tokenizer)
         prompt_len = len(question_ids[0]) - question_len - 1
         question_len = len(question_ids[0])
-        assistant_msg = [{'role':'assistant', 'content':f'Reasoning: {reasoning}\nAnswer: {answer}'}]
+        assistant_msg = [
+            {
+                "role": "assistant",
+                "content": f"Reasoning: {reasoning}\nAnswer: {answer}",
+            }
+        ]
         input_msg = question_msg + assistant_msg
         question_len = question_len - prompt_len
-        stem_len = len(tokenizer(question.split('\n')[0],return_tensors="pt").input_ids[0])
-        stem_len = prompt_len + stem_len
-        cot_msg = question_msg + [{'role':'assistant', 'content':f'{reasoning}'}]
-        cot_len = len(self.interp_build_chat_input(input_msg, tokenizer=tokenizer)[0]) # Get reasoning - Answer paired output
+        cot_msg = question_msg + [{"role": "assistant", "content": f"{reasoning}"}]
+        cot_len = len(
+            self.interp_build_chat_input(input_msg, tokenizer=tokenizer)[0]
+        )  # Get reasoning - Answer paired output
         cot_len = cot_len - prompt_len
         print("COT", cot_len)
-        return {"question_len": question_len, "prompt_len": prompt_len, "input_msg": input_msg, "cot_len": cot_len}
+        return {
+            "question_len": question_len,
+            "prompt_len": prompt_len,
+            "input_msg": input_msg,
+            "cot_len": cot_len,
+        }
