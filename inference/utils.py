c89f072f4d220bf408f72ecd957a069e2046e6b7
ivakhnenko <ivakhnenko@cl.uni-heidelberg.de>
2025-04-21 00:46:12 +0200

    Fix adding messages

--- inference/utils.py
+++ inference/utils.py
@@ -174,8 +174,10 @@ def upd_span(span: tuple, offset: int) -> tuple[int, int]:
     :param offset: offset to add
     :return: updated span
     """
+    if len(span) == 0:
+        return offset, offset
     if len(span) != 2:
-        print("span", span)
+        print("DEBUG: upd span", span)
         raise ValueError(
             "Span must be a tuple of two integers representing the start and end indices."
         )
