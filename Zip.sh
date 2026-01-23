9b99263d362ecc50fea0988cff808e37b7d4649b
kim <kim@tg-lingemann.de>
2026-01-23 22:39:35 +0100

    zip fun

--- Zip.sh
+++ Zip.sh
@@ -21,7 +21,7 @@
 source ~/.bashrc 2>/dev/null
 
 # run bash commands
-zip -r results_baseline.zip /pfs/work9/workspace/scratch/hd_mr338-research-results-2/baseline
-zip -r results_feedback.zip /pfs/work9/workspace/scratch/hd_mr338-research-results-2/feedback
-zip -r results_SD.zip /pfs/work9/workspace/scratch/hd_mr338-research-results-2/SD
-zip -r results_skyline.zip /pfs/work9/workspace/scratch/hd_mr338-research-results-2/skyline
+zip -r /pfs/work9/workspace/scratch/hd_mr338-research-results-2/results_baseline.zip /pfs/work9/workspace/scratch/hd_mr338-research-results-2/baseline
+zip -r /pfs/work9/workspace/scratch/hd_mr338-research-results-2/results_feedback.zip /pfs/work9/workspace/scratch/hd_mr338-research-results-2/feedback
+zip -r /pfs/work9/workspace/scratch/hd_mr338-research-results-2/results_SD.zip /pfs/work9/workspace/scratch/hd_mr338-research-results-2/SD
+zip -r /pfs/work9/workspace/scratch/hd_mr338-research-results-2/results_skyline.zip /pfs/work9/workspace/scratch/hd_mr338-research-results-2/skyline
