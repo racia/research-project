09956773946e56e9b6ebfa1adcae7645338d520f
kim <kim@tg-lingemann.de>
2026-04-27 16:33:36 +0200

    improve eval sh file

--- evaluate_data.sh
+++ evaluate_data.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=evale_data
+#SBATCH --job-name=eval_data
 #SBATCH --output=eval_data_%j.out
 #SBATCH --error=eval_data_%j.err
 #SBATCH --time=02:00:00 # should be enough for 20 samples per task
 #SBATCH --cpus-per-task=4
 # SBATCH --partition=dev_cpu_il
-#SBATCH --mail-user=sari@cl.uni-heidelberg.de
+#SBATCH --mail-user=""
 #SBATCH --mail-type=BEGIN,END,FAIL
 
 setting=$1
@@ -16,7 +16,31 @@ create_heatmaps=$4
 results_path="/pfs/work9/workspace/scratch/hd_mr338-research-results-2/${setting}/test/${task}/v1/all_tasks_joined/joined_reasoning_results.csv"
 save_path="results/${setting}/${task}"
 
-source .env/bin/activate
+### JOB STEPS START HERE ###
+# fix working directory
+cd ~/research-project || exit 1
+
+if command -v module >/dev/null 2>&1; then
+    echo "Module util is available. Loading python and CUDA..."
+    module load devel/python/3.12.3-gnu-14.2
+    module load devel/cuda/12.8
+else
+    echo "Module util is not available. Using manually installed python and CUDA..."
+fi
+
+# initialize shell to work with bash
+source ~/.bashrc 2>/dev/null
+
+# Activate the conda environment
+ENV_NAME=".env"
+echo "Activating the project environment: $ENV_NAME"
+if ! source $ENV_NAME/bin/activate; then
+    echo "Error: Failed to activate the project environment '$ENV_NAME'."
+    exit 1
+else
+    echo "The project environment '$ENV_NAME' activated successfully."
+fi
+
 echo "Evaluating data for setting: $setting, task: $task, samples per task: $samples_per_task, create heatmaps: $create_heatmaps"
 
 if [ "$create_heatmaps" = "true" ]; then
