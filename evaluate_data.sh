1c83eddb8ab7af4ff630415aec854c0e3a45d070
kim <kim@tg-lingemann.de>
2026-04-28 13:46:45 +0200

    add mode to res path

--- evaluate_data.sh
+++ evaluate_data.sh
@@ -14,7 +14,7 @@ task=$2
 samples_per_task=$3
 create_heatmaps=$4
 
-results_path="/pfs/work9/workspace/scratch/hd_mr338-research-results-2/${setting}/${mode}/test/${task}/v1/all_tasks_joined/joined_reasoning_results.csv"
+results_path="/pfs/work9/workspace/scratch/hd_mr338-research-results-2/${setting}/test/${mode}/v1/all_tasks_joined/joined_reasoning_results.csv"
 
 save_path="results/${setting}/${task}"
 
