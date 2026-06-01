d97f49437e39605dc7337da212169d0e21f0d02a
kim <kim@tg-lingemann.de>
2026-06-01 17:31:45 +0200

    eröqoighj

--- scripts/evaluate_data_reasoning.sh
+++ scripts/evaluate_data_reasoning.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=eval_data
 #SBATCH --output=eval_data_%j.out
 #SBATCH --error=eval_data_%j.err
-#SBATCH --time=06:00:00
+#SBATCH --time=12:00:00
 #SBATCH --cpus-per-task=4
 #SBATCH --partition=cpu
 #SBATCH --mail-user=""
