a3b192298ad9f4c193aa7fb6bd2677e03d744816
kim <kim@tg-lingemann.de>
2026-06-12 21:47:28 +0200

    fml

--- scripts/Skyline.sh
+++ scripts/Skyline.sh
@@ -3,12 +3,12 @@
 # Job name
 #SBATCH --job-name=skyline
 
-#SBATCH --time=15:00:00              # Job time limit (30 minutes)
+#SBATCH --time=30:00:00              # Job time limit (30 minutes)
 #SBATCH --ntasks=1                   # Total number of tasks
 #SBATCH --gres=gpu:2                 # Request 2 GPUs
 #SBATCH --cpus-per-task=2            # Number of CPU cores per task
 #SBATCH --mem=32G                    # Total memory requested
-#SBATCH --partition=dev_gpu_4
+#SBATCH --partition=gpu_h100
 
 # Output and error logs
 #SBATCH --output="skyline_out.txt"        # TODO: adjust standard output log
