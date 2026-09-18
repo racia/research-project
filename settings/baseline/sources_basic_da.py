588cad24c88dd5ef3e8f2620f4da423a7c00ecd8
ivakhnenko <ivakhnenko@cl.uni-heidelberg.de>
2026-09-18 16:30:07 +0300

    Add basic baseline configs and source data

---
+++ settings/baseline/sources_basic_da.py
@@ -0,0 +1,39 @@
+basic_baseline_da_v1_t_3 = [
+    "basic-baseline/test/da/v1/unjoined/task_3_s_1_56",
+    "basic-baseline/test/da/v1/unjoined/task_3_s_45_100_task_5_1_11",
+]
+basic_baseline_da_v1_t_5 = [
+    "basic-baseline/test/da/v1/unjoined/task_4_full_task_5_1_45",
+    "basic-baseline/test/da/v1/unjoined/t_5_s_46_100_t_6_20_all",
+]
+basic_baseline_da_v1 = [
+    "basic-baseline/test/da/v1/task_1_2",
+    *[f"basic-baseline/test/da/v1/task_{i}" for i in range(3, 21)],
+]
+basic_baseline_da_v2 = [
+    "basic-baseline/test/da/v2/task_1_2",
+    "basic-baseline/test/da/v2/task_3",
+    "basic-baseline/test/da/v2/task_4_20",
+]
+basic_baseline_da_v3 = [
+    "basic-baseline/test/da/v3/task_1_2",
+    "basic-baseline/test/da/v3/task_3",
+    "basic-baseline/test/da/v3/task_4_14",
+    "basic-baseline/test/da/v3/task_14_20",
+]
+basic_baseline_da_v4_t_13 = [
+    "basic-baseline/test/da/v4/task_4_12_full_task_13_s_1_75",
+    "basic-baseline/test/da/v4/task_13_76_100_task_14_20_full",
+]
+basic_baseline_da_v4 = [
+    "basic-baseline/test/da/v4/task_1_2",
+    "basic-baseline/test/da/v4/task_3",
+    "basic-baseline/test/da/v4/task_4_12_full_task_13_s_1_75",
+    "basic-baseline/test/da/v4/task_13",
+    "basic-baseline/test/da/v4/task_13_76_100_task_14_20_full",
+]
+basic_baseline_da_v5 = [
+    "basic-baseline/test/da/v5/task_1_2",
+    "basic-baseline/test/da/v5/task_3",
+    "basic-baseline/test/da/v5/task_4_20",
+]
